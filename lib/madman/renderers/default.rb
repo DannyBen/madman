e52acbb06458e4c7a2c51c95bc4ee23947d25508
Danny Ben Shitrit <db@dannyben.com>
2020-06-25 16:15:15 +0000

    allow unsafe html

--- lib/madman/renderers/default.rb
+++ lib/madman/renderers/default.rb
@@ -2,7 +2,7 @@ module Madman
   module Renderers
     class Default
       def self.render(text, opts={})
-        CommonMarker.render_html text, :DEFAULT, [:table]
+        CommonMarker.render_html text, [:DEFAULT, :UNSAFE], [:table]
       end
     end
   end
--- spec/madman/renderers/default_spec.rb
+++ spec/madman/renderers/default_spec.rb
@@ -7,5 +7,10 @@ describe Renderers::Default do
     it 'returns HTML' do
       expect(subject.render '# hello world').to match_approval('renderer/default')
     end
+
+    it "allows arbitrary html" do
+      expect(subject.render "# hello\n\n<p style='background: #fff'></p>")
+        .to match_approval('renderer/default-htmlcode')
+    end
   end
 end
