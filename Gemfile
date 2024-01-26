6b9079ceba7e97839c162d9d9cea19d9920b3f37
Danny Ben Shitrit <db@dannyben.com>
2024-01-26 06:40:15 +0000

    - Drop support for Ruby 3.0

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ gem 'debug', require: ENV['DEBUGGER'] == '1'
 gem 'lp'
 gem 'rack-test'
 gem 'rspec'
-gem 'rspec-html-matchers'
 gem 'rspec_approvals'
+gem 'rspec-html-matchers'
 gem 'runfile', require: false
 gem 'runfile-tasks', require: false
 gem 'simplecov'
--- lib/madman/renderers/default.rb
+++ lib/madman/renderers/default.rb
@@ -3,8 +3,8 @@ module Madman
     class Default
       def self.render(text, _opts = {})
         Commonmarker.to_html text, options: {
-          render: { unsafe: true, },
-          extension: { table: true }
+          render:    { unsafe: true },
+          extension: { table: true },
         }
       end
     end
